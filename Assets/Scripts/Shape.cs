960a6af4a1f6db19fa44431f8d138b29dea9bb68
JaydenSky <85710954+JaydenSky@users.noreply.github.com>
2021-06-21 08:31:15 -0600

    Added Correct Files
    Put in the files it needed

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    
+    public virtual void Move()
+    {
+
+    }
+
+    public void SetColor()
+    {
+
+    }
+}
