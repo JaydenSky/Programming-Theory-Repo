657bb7378b2a777803387c00e1dc03d5889f1987
JaydenSky <85710954+JaydenSky@users.noreply.github.com>
2021-06-23 16:45:46 -0600

    Encapsulation implemented
    Added a getter and setter for the shape color

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    public string shapeColor;
+    private string m_shapeColor;
+
+    //ENCAPSULATION
+    public string shapeColor
+    {
+        get { return m_shapeColor; }
+        set
+        {
+            if (value == "Red" || value == "Blue" || value == "Green")
+            {
+                m_shapeColor = value;
+            }
+            else
+            {
+                Debug.LogError("An invalid color, " + value + ", was put in");
+            }
+        }
+    }
 
     public MeshRenderer Renderer;
-
-    private void Start()
-    {
-        
-    }
 
     public virtual void Move()
     {
@@ -20,15 +32,15 @@ public class Shape : MonoBehaviour
 
     public void SetColor()
     {
-        if (shapeColor == "Red")
+        if (m_shapeColor == "Red")
         {
             Renderer.material.color = new Color(202, 0, 0);
         }
-        else if (shapeColor == "Blue")
+        else if (m_shapeColor == "Blue")
         {
             Renderer.material.color = new Color(0, 0, 202);
         }
-        else if (shapeColor == "Green")
+        else if (m_shapeColor == "Green")
         {
             Renderer.material.color = new Color(0, 202, 0);
         }
